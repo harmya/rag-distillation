7fe9135561f47a1cac5f1b1c4c798ffa60544cc1
harmya <hvbhatt@purdue.edu>
2024-04-26 11:37:47 -0400

    Added new code, copied files to ssh server

---
+++ src/eval.py
@@ -0,0 +1,28 @@
+from transformers import BertForQuestionAnswering, BertTokenizer
+import torch
+
+
+model_path = 'student_epoch_9.pt'
+
+def load_model(model_path):
+    model = BertForQuestionAnswering.from_pretrained("bert-base-uncased")
+    model.load_state_dict(torch.load(model_path))
+    model.eval()
+    return model
+
+def answer_question(question, context, model):
+    tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
+    inputs = tokenizer(question, context, return_tensors="pt")
+    input_ids = inputs["input_ids"].tolist()[0]
+    outputs = model(**inputs)
+    answer_start = torch.argmax(outputs.start_logits)
+    answer_end = torch.argmax(outputs.end_logits) + 1
+    answer = tokenizer.convert_tokens_to_string(tokenizer.convert_ids_to_tokens(input_ids[answer_start:answer_end]))
+    return answer
+
+
+question = "What is the capital of France?"
+context = "The capital of France is Paris."
+
+model = load_model(model_path)
+answer = answer_question(question, context, model)
