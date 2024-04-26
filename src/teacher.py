4bd14b4d800c7492fae7659e9dfb8c54700d32b6
harmya <hvbhatt@purdue.edu>
2024-04-26 02:37:20 -0400

    done

--- src/teacher.py
+++ src/teacher.py
@@ -2,7 +2,7 @@ from transformers import BertForQuestionAnswering, BertTokenizer
 
 class Teacher:
     def __init__(self):
-        self.model = BertForQuestionAnswering.from_pretrained('bert-large-uncased-whole-word-masking-finetuned-squad')
+        self.model = BertForQuestionAnswering.from_pretrained('bert-small-finetuned-squad')
         self.tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
         print("Teacher model initialized.")
         print(f"Total parameters: {self.model.num_parameters()}")
