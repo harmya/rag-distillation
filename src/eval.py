0b47ef92d526f632ccc3a03adfd14ed0371c1a4a
harmya <hvbhatt@purdue.edu>
2024-04-26 17:21:31 -0400

    Added new code, copied files to ssh server

--- src/eval.py
+++ src/eval.py
@@ -26,8 +26,8 @@ def answer_question(question, context, model):
     answer = tokenizer.convert_tokens_to_string(tokenizer.convert_ids_to_tokens(input_ids[answer_start:answer_end]))
     return answer
 
-question = "What is the capital of France?"
-context = "The capital of France is Paris."
+question = "An igneous rock is a rock that crystallizes from what?"
+context = ""
 
 model = load_model(model_path)
 answer = answer_question(question, context, model)
