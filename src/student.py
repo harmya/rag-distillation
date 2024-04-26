16c5b3e88c2a5b3ac709b1d0708989dc8f3f5698
harmya <hvbhatt@purdue.edu>
2024-04-26 02:16:00 -0400

    lil bro
    added start and end logits
    
    done
    
    nah
    
    hi
    
    lel
    
    he
    
    real
    
    me
    
    ok

--- src/student.py
+++ src/student.py
@@ -1,12 +1,12 @@
 from transformers import BertConfig, BertForQuestionAnswering
 
 class Student:
-    def __init__(self, num_hidden_layers=6, num_attention_heads=12):
+    def __init__(self, num_hidden_layers=6, num_attention_heads=6):
         self.config = BertConfig(
-            hidden_size=512,
+            hidden_size=num_attention_heads * 64,
             num_hidden_layers=num_hidden_layers,
             num_attention_heads=num_attention_heads,
-            intermediate_size=1024,
+            intermediate_size=512,
         )
         self.config.num_hidden_layers = num_hidden_layers
         self.config.num_attention_heads = num_attention_heads
