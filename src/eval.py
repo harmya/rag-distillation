3e1e06195b33390c59effbede4d414c3fc606678
harmya <hvbhatt@purdue.edu>
2024-04-26 17:19:54 -0400

    Added new code, copied files to ssh server

--- src/eval.py
+++ src/eval.py
@@ -7,8 +7,8 @@ model_path = '../models/student_epoch_9.pt'
 def load_model(model_path):
     state_dict = torch.load(model_path)
     for key in list(state_dict.keys()):
-        if 'model.' in key:
-            state_dict[key.replace('model.', '')] = state_dict.pop(key)
+        if 'module.' in key:
+            state_dict[key.replace('module.', '')] = state_dict.pop(key)
     student = Student()
     model = student.model
     model.load_state_dict(state_dict)
