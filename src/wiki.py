e8772b37a6edc0e77bfba42d939efe90a562e4c9
harmya <hvbhatt@purdue.edu>
2024-04-27 17:07:01 -0400

    Added new code, copied files to ssh server

---
+++ src/wiki.py
@@ -0,0 +1,36 @@
+from datasets import load_dataset
+import torch
+import cohere
+import os
+import dotenv  
+
+dotenv.load_dotenv()
+
+
+co = cohere.Client(f"{os.getenv('COHERE_API_KEY')}")
+
+max_docs = 1000
+docs_stream = load_dataset(f"Cohere/wikipedia-22-12-en-embeddings", split="train", streaming=True)
+
+docs = []
+doc_embeddings = []
+
+for doc in docs_stream:
+    docs.append(doc)
+    doc_embeddings.append(doc['emb'])
+    if len(docs) >= max_docs:
+        break
+
+doc_embeddings = torch.tensor(doc_embeddings)
+
+query = "What is the capital of France?"
+response = co.embed(texts=[query], model='multilingual-22-12')
+query_embedding = torch.tensor(response.embeddings)
+
+similarity = torch.matmul(query_embedding, doc_embeddings.T)
+most_similar_idx = torch.argmax(similarity)
+
+most_similar_doc = docs[most_similar_idx]
+most_similar_text = most_similar_doc['text']
+
+print(most_similar_text)
