3c8fb25ca23ba4283b8d331aa47ef0f912858fd9
harmya <hvbhatt@purdue.edu>
2024-04-28 04:26:22 -0400

    Added new code, copied files to ssh server

--- src/wiki.py
+++ src/wiki.py
@@ -1,34 +1,35 @@
-from datasets import load_dataset
 import torch
 import cohere
 import os
-import dotenv  
+import dotenv
 import dataloader
+from torch.utils.data import DataLoader
+from datasets import load_dataset
 
 dotenv.load_dotenv()
 
+
 co = cohere.Client(f"{os.getenv('COHERE_PROD')}")
 
-docs_stream = load_dataset(f"Cohere/wikipedia-22-12-simple-embeddings", split="train", streaming=True)
+dataset = load_dataset("Cohere/wikipedia-22-12-simple-embeddings", split="train", streaming=True)
 
 docs = []
 doc_embeddings = []
 loaded = 0
-
-for doc in docs_stream:
+for doc in dataset:
     docs.append(doc)
     doc_embeddings.append(doc['emb'])
     loaded += 1
     print(f"Loaded {loaded} documents", end="\r")
 
-doc_embeddings = torch.tensor(doc_embeddings)
+doc_embeddings = torch.cat(doc_embeddings, dim=0).to('cuda')
 
 rag_outputs = {}
 queries = dataloader.get_questions()
 
 for query_id, query in queries.items():
     response = co.embed(texts=[query], model='multilingual-22-12')
-    query_embedding = torch.tensor(response.embeddings)
+    query_embedding = torch.tensor(response.embeddings).to('cuda')
 
     similarity = torch.matmul(query_embedding, doc_embeddings.T)
     most_similar_idx = torch.argmax(similarity)
